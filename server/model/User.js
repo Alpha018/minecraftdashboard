f683e3e28fcc1ce1ee6c93a5ce0dc7e0b5f8a974
alpha018 <tas001@alumos.ucn.cl>
2018-11-03 22:22:23 -0300

    Added: more .env variables

--- server/model/User.js
+++ server/model/User.js
@@ -11,7 +11,7 @@ const userSchema = new Schema({
         required: true,
         select: false,
     },
-    // Solo persona
+
     email: {
         type: String,
         trim: true,
@@ -20,7 +20,7 @@ const userSchema = new Schema({
             return /^[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)
         },'El email utilizado no es valido.']
     },
-    //Nombre Completo
+
     username: {
         type: String,
         required:true,
